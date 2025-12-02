79e86b463b174e50656cf142b25f037b7319cfbe
Gabriele Rosso <gabriele.rosso@edu.unito.it>
2025-12-02 11:55:47 +0100

    Added spike init

---
+++ gpt_spike/src/main/java/org/example/gpt_spike/GptSpikeApplication.java
@@ -0,0 +1,13 @@
+package org.example.gpt_spike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptSpikeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GptSpikeApplication.class, args);
+  }
+
+}
---
+++ gpt_spike/src/test/java/org/example/gpt_spike/GptSpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gpt_spike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptSpikeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
